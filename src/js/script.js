e85f319f74e96941e58a7c9e0cd1ad34d67c3695
Victor <victoraltieri1@gmail.com>
2025-03-28 11:42:35 -0300

    Como declarar uma variável

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+// declaração de variáveis
+
+var nome = "Fiap";
+console.log(nome)
+
+let idade = 10
+console.log(idade)
+
+const sobrenome = "Fiapinho"
+console.log(sobrenome)
+
+//undefined
+
+let exemplo;
+console.log(exemplo)
