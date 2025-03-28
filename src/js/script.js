7dec9b099306cda380a11be7ccc6730459c4f42f
Victor <victoraltieri1@gmail.com>
2025-03-28 11:49:39 -0300

    Tipos de variáveis no JS

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,25 @@ console.log(sobrenome)
 
 let exemplo;
 console.log(exemplo)
+
+//null
+
+let exemplo2 = null;
+console.log(exemplo2)
+
+// tipos de variáveis
+
+let ex1 = "Fiap";
+console.log(typeof ex1)
+
+let ex2 = 25;
+console.log(typeof ex2)
+
+let ex3 = true
+console.log(typeof ex3)
+
+let ex4 = {}
+console.log(typeof ex4)
+
+let ex5 = []
+console.log(typeof ex5)
